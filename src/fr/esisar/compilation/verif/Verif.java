e8169ac8dbe773f80dfbef26ae9450ca44a2fad4
userir <userir@debian>
2018-11-15 10:25:10 +0100

    rectification des infocodepour la partie code

--- src/fr/esisar/compilation/verif/Verif.java
+++ src/fr/esisar/compilation/verif/Verif.java
@@ -324,7 +324,8 @@ public class Verif {
 			
 			/* Décoration du noeud avec le type */
 			Decor decAff = new Decor(getTypeNoeud(a.getFils1())) ;
-			decAff.setInfoCode(a.getFils1().getDecor().getInfoCode()+a.getFils2().getDecor().getInfoCode());
+			decAff.setInfoCode(a.getFils2().getDecor().getInfoCode());
+			//System.out.println("Infocode ajouté : " + a.getFils1().getDecor().getInfoCode() + " et " + a.getFils2().getDecor().getInfoCode());
 			a.setDecor(decAff);			
 			break;
 			
@@ -466,7 +467,7 @@ public class Verif {
 		/* Décorer le noeud si nécessaire */
 		if(setDecor) {
 			Decor dec = new Decor(res_index.getTypeRes()) ; 
-			dec.setInfoCode(1);
+			dec.setInfoCode(a.getFils1().getDecor().getInfoCode()+a.getFils2().getDecor().getInfoCode()); // finalement on le calcule déja dans ident
 			a.setDecor(dec);
 		}
 		
@@ -679,7 +680,9 @@ public class Verif {
 		/* Créer le noeud */
 		Arbre conv = Arbre.creation1(Noeud.Conversion, a.getFils(numFils), a.getNumLigne());
 		/* Le décorer */
-		conv.setDecor(new Decor(Type.Real));
+		Decor decConv = new Decor(Type.Real);
+		decConv.setInfoCode( a.getFils(numFils).getDecor().getInfoCode());
+		conv.setDecor(decConv);
 		/* Le positionner */
 		a.setFils(numFils, conv);
 	}
