abc33d257eb3f8178ce72e805d6997f1d5f51488
userir <userir@debian>
2018-11-29 14:30:09 +0100

    Version finale du For BUENO

--- src/fr/esisar/compilation/gencode/CoderProg.java
+++ src/fr/esisar/compilation/gencode/CoderProg.java
@@ -245,62 +245,70 @@ public class CoderProg {
 			break;
 
 		case Pour:
-			Registre r = allouerRegistre();
+			//	Registre r = allouerRegistre();
+			Registre rdebut = allouerRegistre();
+			Registre rfin = allouerRegistre();
 
-			int debut = 0; // récuperer le premier entier
-			int fin = 0; // récuperer le dernier entier
+			coder_EXP(a.getFils1().getFils2(), rdebut);
+			coder_EXP(a.getFils1().getFils3(), rfin);
+
+
+			int offset2 = coder_PLACE(a.getFils1().getFils1());
+
+
 			if(a.getFils1().getNoeud().equals(Noeud.Increment)) {		
-				
+
 				/******************************************************
 				IL FAUT REGARDER COMMENT ON A IMPLEMENTER INCREMENT ET DECREMENT DANS LA PASSE 2 
-				
+
 				IL FAUT GERER LUTILISATION DE L'IDENTIFICATEUR DU FOR DANS LA LISTE D'INSTRUCTION 
-				******************************************************/
-				
-				debut  = a.getFils1().getFils2().getEntier();
-				fin  =  a.getFils1().getFils3().getEntier();
-				
-				
-				Prog.ajouter(Inst.creation2(Operation.LOAD, Operande.creationOpEntier(debut), Operande.opDirect(r))); // LOAD R <- DEBUT 
+				 ******************************************************/
+
+
+
+
 				Etiq etiqFOR = Etiq.nouvelle("FOR") ;
 				Prog.ajouter( etiqFOR, "Etiquette pour le FOR"); // FOR :
-				Prog.ajouter(Inst.creation2(Operation.CMP, Operande.creationOpEntier(fin), Operande.opDirect(r)), "Comparer le registre et le 10"); // CMP R, 10 
-	
+				Prog.ajouter(Inst.creation2(Operation.STORE, Operande.opDirect(rdebut), Operande.creationOpIndirect(offset2, Registre.GB)));
+
+				Prog.ajouter(Inst.creation2(Operation.CMP, Operande.opDirect(rfin), Operande.opDirect(rdebut)), "Comparer le registre et le 10"); // CMP R, 10 
+
 				Etiq etiqEXIT = Etiq.nouvelle("EXIT") ; 
 				Prog.ajouter(Inst.creation1(Operation.BGT, Operande.creationOpEtiq(etiqEXIT))); // Branch vers Exit si R>10 
-				Prog.ajouter(Inst.creation2(Operation.ADD, Operande.creationOpEntier(1), Operande.opDirect(r))); // ADD R , 1
-
+				Prog.ajouter(Inst.creation2(Operation.ADD, Operande.creationOpEntier(1), Operande.opDirect(rdebut))); // ADD R , 1
+				coder_TEST_ADD_OV();
 				coder_LISTE_INST(a.getFils2()); // coder la liste d'instruction
 
 				Prog.ajouter(Inst.creation1(Operation.BRA, Operande.creationOpEtiq(etiqFOR))); // Branch vers For
-				
+
 				Prog.ajouter( etiqEXIT, " L'etiquette de la fin du POUR") ; // EXIT : 
-				
-				
-				
+
+
+
 			}else if(a.getFils1().getNoeud().equals(Noeud.Decrement)) {
-				
-				debut = a.getFils1().getFils2().getEntier();
-				fin =  a.getFils1().getFils3().getEntier();
-				
-				Prog.ajouter(Inst.creation2(Operation.LOAD, Operande.creationOpEntier(debut), Operande.opDirect(r))); // LOAD R <- DEBUT 
+
+
 				Etiq etiqFOR = Etiq.nouvelle("FOR") ;
 				Prog.ajouter( etiqFOR, "Etiquette pour le FOR"); // FOR :
-				Prog.ajouter(Inst.creation2(Operation.CMP, Operande.creationOpEntier(fin), Operande.opDirect(r)), "Comparer le registre et le 10"); // CMP R, 10 
-	
+				Prog.ajouter(Inst.creation2(Operation.STORE, Operande.opDirect(rdebut), Operande.creationOpIndirect(offset2, Registre.GB)));
+
+
+				Prog.ajouter(Inst.creation2(Operation.CMP, Operande.opDirect(rfin), Operande.opDirect(rdebut)), "Comparer le registre et le 10"); // CMP R, 10 
+
 				Etiq etiqEXIT = Etiq.nouvelle("EXIT") ; 
 				Prog.ajouter(Inst.creation1(Operation.BLT, Operande.creationOpEtiq(etiqEXIT))); // Branch vers Exit si R<10 
-				Prog.ajouter(Inst.creation2(Operation.SUB, Operande.creationOpEntier(1), Operande.opDirect(r))); // ADD R , 1
-
+				Prog.ajouter(Inst.creation2(Operation.SUB, Operande.creationOpEntier(1), Operande.opDirect(rdebut))); // ADD R , 1
+				coder_TEST_ADD_OV();
 				coder_LISTE_INST(a.getFils2()); // coder la liste d'instruction
 
 				Prog.ajouter(Inst.creation1(Operation.BRA, Operande.creationOpEtiq(etiqFOR))); // Branch vers For
-				
+
 				Prog.ajouter( etiqEXIT, " L'etiquette de la fin du POUR") ; // EXIT : 
 			}
 
-			
-			
+			libererRegistre(rdebut);
+			libererRegistre(rfin);
+
 			break;
 
 		case TantQue:
